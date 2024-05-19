b219a7ea31e883e2c5b28d57c5f7ea634f3965f7
Ps4Atom <pranav2097@gmail.com>
2024-05-19 23:08:30 +0530

    a new portfolio using nextjs

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems = [
+    {title: 'Title', description: 'Desc', id:1},
+    {title: 'Title', description: 'Desc', id:1},
+    {title: 'Title', description: 'Desc', id:1},
+    {title: 'Title', description: 'Desc', id:1},
+]
